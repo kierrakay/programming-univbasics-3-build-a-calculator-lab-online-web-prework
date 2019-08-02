f09d26a66a891d0ef55318872a77706e78cf82c5
Kierra M <k.merriman32@hotmail.com>
2019-08-02 23:34:46 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  subtract = num,num2
+  return num1-num2
 end
 
 def division(num1, num2)
-
+  division = num1,num2
+return num1/num2
 end
 
 def multiplication(num1, num2)
-
+  multiplication = num1,num2
+return num1*num2
 end
 
 def modulo(num1, num2)
